37c6c68ce634168f0cc7defa0c8ee9ab3c7032bf
Marcos Nathan <mnengcomp@gmail.com>
2026-01-23 10:18:11 -0300

    chore: remove comments

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -10,21 +10,14 @@ plugins {
 
 dependencies {
     //#ktor
-    //api("io.ktor:ktor-client-core-jvm:3.0.0")
     api(libs.ktor.client.core)
-    //api("io.ktor:ktor-client-cio-jvm:3.0.0")
     api(libs.ktor.client.cio)
-    //api("io.ktor:ktor-serialization-kotlinx-json-jvm:3.0.0")
     api(libs.ktor.client.serialization)
-    //api("io.ktor:ktor-client-content-negotiation-jvm:3.0.0")
     api(libs.ktor.client.content.negotiation)
-
     api(libs.ktor.client.logging)
 
 
-    //testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation(kotlin("test"))
-    //testImplementation("io.ktor:ktor-client-mock:3.0.0")
     testImplementation(libs.ktor.client.mock)
 }
 
