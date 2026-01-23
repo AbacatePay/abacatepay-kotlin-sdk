d270ccb84e740f8d9470a8ddeafa8c0452dcd3c7
Marcos Nathan <mnengcomp@gmail.com>
2026-01-23 11:13:18 -0300

    chore: update command-line sample to use composite build

--- samples/command-line/src/main/kotlin/com/abacatepay/Main.kt
+++ samples/command-line/src/main/kotlin/com/abacatepay/Main.kt
@@ -1,6 +1,19 @@
+package com.abacatepay
+
 import com.abacatepay.client.AbacatePayClient
+import com.abacatepay.client.AbacatePayOptions
+import io.ktor.client.plugins.logging.LogLevel
 
 suspend fun main() {
-    val abacatePayClient = AbacatePayClient(apiKey = "your_key")
-    abacatePayClient.listBillings()
+    val abacatePayClient = AbacatePayClient(
+        apiKey = "abc_dev_CAL0sZM21NHtcJ1GjCxcUmTB",
+        options = AbacatePayOptions(
+            logLevel = LogLevel.INFO
+        ),
+    )
+    val response = abacatePayClient.billing.list()
+    response.fold(
+        onSuccess = { println("success: $it") },
+        onFailure = { println("failure: $it") }
+    )
 }
